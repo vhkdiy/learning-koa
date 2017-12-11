ab42903ded3556b3f43a55de17d2b3910bdc6dce
vhkdiy <404924175@qq.com>
2017-12-11 23:29:44 +0800

    middleware and koa-router

---
+++ 02-middleware/app.js
@@ -0,0 +1,34 @@
+const Koa = require('koa')
+const app = new Koa()
+
+//记录执行时间
+app.use(async (ctx, next) => {
+    let stime = new Date().getTime()
+    await next()
+    let etime = new Date().getTime()
+    ctx.response.type = 'text/html'
+    ctx.response.body = '<h1>Hello World!</h1>'
+    console.log(`请求地址：${ctx.path}, 响应时间：${etime - stime}ms`)
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件1 start')
+    await next()
+    console.log('中间件1 end')
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件2 start')
+    // await next()
+    console.log('中间件2 end')
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件3 start')
+    await next()
+    console.log('中间件3 end')
+})
+
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000')
+})
