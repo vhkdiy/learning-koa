7246c22f8626dc8be0796799da2614e3f098af04
vhkdiy <404924175@qq.com>
2017-12-15 23:13:57 +0800

    json-middleware

---
+++ 08-json/middleware/index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const staticFiles = require('koa-static')
+const bodyParser = require('koa-bodyparser')
+const nunjucks = require('koa-nunjucks-2')
+const sendJson = require('./send-json')
+
+module.exports = (app) => {
+    app.use(staticFiles(path.resolve(__dirname, "../public"))) // 设置静态资源目录
+
+    app.use(nunjucks({
+        ext: 'html',
+        path: path.join(__dirname, '../views'),// 指定视图目录
+        nunjucksConfig: {
+            trimBlocks: true // 开启转义 防Xss
+        }
+    }))
+
+    app.use(bodyParser())
+    app.use(sendJson())
+}
