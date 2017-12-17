fff225139b843773addd606c0b5072ce4f7eff8e
vhkdiy <404924175@qq.com>
2017-12-17 23:33:58 +0800

    logger

--- 09-log-middleware/middleware/index.js
+++ 09-log-middleware/middleware/index.js
@@ -3,10 +3,11 @@ const staticFiles = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const nunjucks = require('koa-nunjucks-2')
 const sendJson = require('./send-json')
+const miLog = require('./mi-log')
 
 module.exports = (app) => {
+    app.use(miLog())
     app.use(staticFiles(path.resolve(__dirname, "../public"))) // 设置静态资源目录
-
     app.use(nunjucks({
         ext: 'html',
         path: path.join(__dirname, '../views'),// 指定视图目录
--- 09-log-middleware/middleware/mi-log/demo.js
+++
@@ -1,4 +0,0 @@
-const log4js = require('log4js');
-const logger = log4js.getLogger();
-logger.level = 'debug';
-logger.debug("Some debug messages");
