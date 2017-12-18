7c81ceb9ecd48a659c402fe7864618e8dbe4ff31
vhkdiy <404924175@qq.com>
2017-12-18 23:34:11 +0800

    mi-log

--- 09-log-middleware/middleware/index.js
+++ 09-log-middleware/middleware/index.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const ip = require('ip')
 const staticFiles = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const nunjucks = require('koa-nunjucks-2')
@@ -6,7 +7,13 @@ const sendJson = require('./send-json')
 const miLog = require('./mi-log')
 
 module.exports = (app) => {
-    app.use(miLog())
+    app.use(miLog({
+        env: app.env,  // koa 提供的环境变量
+        projectName: 'koa2-tutorial',
+        appLogLevel: 'debug',
+        dir: 'logs',
+        serverIp: ip.address()
+    }))
     app.use(staticFiles(path.resolve(__dirname, "../public"))) // 设置静态资源目录
     app.use(nunjucks({
         ext: 'html',
